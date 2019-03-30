f56bc495cbb5cd0d21343b29a7d0d9d46cd2b507
unknown <ashweenmankash@gmail.com>
2019-03-30 08:51:05 -0700

    First Commit

---
+++ android/app/src/main/java/ooo/shyft/shyft_packers_and_movers/MainActivity.java
@@ -0,0 +1,13 @@
+package ooo.shyft.shyft_packers_and_movers;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
