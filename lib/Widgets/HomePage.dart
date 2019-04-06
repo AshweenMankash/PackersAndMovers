419a93308d0580436a692e91fc35e6a40cf5a7e3
Ashween Mankash <ashweenmankash@gmail.com>
2019-04-06 20:57:58 +0530

    Committing Safe Build For Last time

--- lib/Widgets/HomePage.dart
+++ lib/Widgets/HomePage.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shyft_packers_and_movers/BLoc/ShyftBloc.dart';
 import 'package:shyft_packers_and_movers/BlocProvider/LoginBLocProvider.dart';
@@ -40,85 +42,88 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   @override
   Widget build(BuildContext context) {
 
-              return StreamBuilder(
-                  stream: _bLoc.homePageBloc.moving(),
-                  builder: (context, snapshot) {
-                      return Center(
-                        child: snapshot.hasData?(!snapshot.data)?Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  "Select Service",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.0,
-                                      color: Colors.black54),
-                                ),
-                              ),
-                              Flex(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  InkWell(
-                                    child: Stack(
-                                      children: <Widget>[
-                                        Padding(
-                                          padding: const EdgeInsets.all(30.0),
-                                          child: Container(
-                                            width: MediaQuery.of(context).size.width,
-                                            height: MediaQuery.of(context).size.height * 0.2,
-                                            child: Image.network(
-                                              "https://upload.wikimedia.org/wikipedia/commons/5/53/Les_deux_amis_ayant_aid%C3%A9_%C3%A0_l%27emm%C3%A9nagement_du_troisi%C3%A8me.jpg",
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.all(30.0),
-                                          child: Container(
-                                            color: Color.fromRGBO(0, 0, 0, 0.4),
-                                            width: MediaQuery.of(context).size.width,
-                                            height: MediaQuery.of(context).size.height * 0.2,
-                                            padding: EdgeInsets.all(20.0),
-                                            alignment: Alignment.bottomRight,
-                                            child: Text(
-                                              "Moving Home",
-                                              style: TextStyle(fontSize: 25.0, color: Colors.white),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    onTap: () {
-                                      Navigator.push(context,
-                                          MaterialPageRoute(builder: (ctx) => MovingHomesWidget()));
-                                    },
+              return FutureBuilder(
+                  future: FirebaseAuth.instance.currentUser(),
+                  builder: (context, user) {
+                      return StreamBuilder(
+                        stream: Firestore.instance.document("Users/${user.data?.uid}").snapshots(),
+                        builder: (context, snapshot) {
+                          return snapshot.hasData?(!snapshot.data["isShyfting"])?Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text(
+                                    "Select Service",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.0,
+                                        color: Colors.black54),
                                   ),
-                                ],
-                                direction: Axis.vertical,
-                              ),
-                              Expanded(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(20.0),
-                                    child: Container(
-                                      child: Text(
-                                        "You have a shyfting going on",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20.0,
-                                            color: Colors.black54),
+                                ),
+                                Flex(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    InkWell(
+                                      child: Stack(
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.all(30.0),
+                                            child: Container(
+                                              width: MediaQuery.of(context).size.width,
+                                              height: MediaQuery.of(context).size.height * 0.2,
+                                              child: Image.network(
+                                                "https://upload.wikimedia.org/wikipedia/commons/5/53/Les_deux_amis_ayant_aid%C3%A9_%C3%A0_l%27emm%C3%A9nagement_du_troisi%C3%A8me.jpg",
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(30.0),
+                                            child: Container(
+                                              color: Color.fromRGBO(0, 0, 0, 0.4),
+                                              width: MediaQuery.of(context).size.width,
+                                              height: MediaQuery.of(context).size.height * 0.2,
+                                              padding: EdgeInsets.all(20.0),
+                                              alignment: Alignment.bottomRight,
+                                              child: Text(
+                                                "Moving Home",
+                                                style: TextStyle(fontSize: 25.0, color: Colors.white),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
                                       ),
+                                      onTap: () {
+                                        Navigator.push(context,
+                                            MaterialPageRoute(builder: (ctx) => MovingHomesWidget()));
+                                      },
                                     ),
-                                  ))
-                            ],
-                          ),
-                        ):MovingHomeStatusWidget():Container(
-                          width: 50,
-                          height: 50,
-                          child: CircularProgressIndicator(),
-                        ),
+                                  ],
+                                  direction: Axis.vertical,
+                                ),
+                                Expanded(
+                                    child: Padding(
+                                      padding: EdgeInsets.all(20.0),
+                                      child: Container(
+                                        child: Text(
+                                          "You have a shyfting going on",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20.0,
+                                              color: Colors.black54),
+                                        ),
+                                      ),
+                                    ))
+                              ],
+                            ),
+                          ):MovingHomeStatusWidget():Container(
+                            width: 50,
+                            height: 50,
+                            child: CircularProgressIndicator(),
+                          );
+                        }
                       );
                     }
               );
--- lib/Widgets/movingHomeStatusWidget.dart
+++ lib/Widgets/movingHomeStatusWidget.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shyft_packers_and_movers/BLoc/ShyftBloc.dart';
 import 'package:shyft_packers_and_movers/BlocProvider/LoginBLocProvider.dart';
+import 'package:shyft_packers_and_movers/Model/MovingServiceModel.dart';
 
 class MovingHomeStatusWidget extends StatefulWidget {
   @override
@@ -24,41 +26,115 @@ class _MovingHomeStatusWidgetState extends State<MovingHomeStatusWidget> {
         initialData: true,
         stream: _shyftBLoc.movingHomeStatusBLoc.load,
         builder: (context, snapshot) {
-          return !snapshot.data
-              ? Center(
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    child: CircularProgressIndicator(),
-                  ),
-                )
-              : Container(
-                  padding: EdgeInsets.all(20.0),
-                  child: StreamBuilder<QuerySnapshot>(
-                      stream: _shyftBLoc.movingHomeStatusBLoc.currentMovingData,
-                      builder: (context, movingData) {
-                        return movingData.hasData
-                            ? Material(
-                                borderRadius: BorderRadius.circular(20.0),
-                                elevation: 5.0,
+          return FutureBuilder<FirebaseUser>(
+            future: FirebaseAuth.instance.currentUser(),
+            builder: (context, user) {
+              return !snapshot.data
+                  ? Center(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: StreamBuilder<QuerySnapshot>(
+                          stream: Firestore.instance.collection("work/${user.data?.uid}/currentMoving").snapshots(),
+                          builder: (context, movingData) {
+                            MovingServiceModel movingServiceModel;
+                            if (movingData.hasData) {
+                              movingServiceModel = MovingServiceModel.fromJson(
+                                  movingData.data.documents[0]);
+                              return Material(
+                                borderRadius: BorderRadius.circular(10.0),
+                                elevation: 0.0,
                                 child: Container(
-                                  padding: EdgeInsets.all(100.0),
+                                  padding: EdgeInsets.all(20.0),
                                   child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
                                     mainAxisSize: MainAxisSize.min,
                                     children: <Widget>[
-                                      Text(movingData.data.documents.length >= 1
-                                          ? movingData.data.documents[0]
-                                              ["pickUpAddress"]
-                                          : "")
+
+                                      Material(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        elevation: 2.0,
+                                        child: Container(
+                                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: Colors.black54,),
+                                          padding: const EdgeInsets.all(10.0),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: <Widget>[
+                                              Text("Shifting Date   "+movingServiceModel.date.day.toString()+"/"+movingServiceModel.date.month.toString()+"/"+movingServiceModel.date.year.toString(),style: TextStyle(color:Colors.white),),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: <Widget>[
+
+                                          Text(movingServiceModel.pickUpAddress.toUpperCase(),style: TextStyle(fontWeight: FontWeight.w300,fontSize: 20.0),semanticsLabel: "PickUpLocation",),
+                                          Icon(Icons.linear_scale,color: Colors.blue,size: 50,),
+                                          Text(movingServiceModel.dropAddress.toUpperCase(),style: TextStyle(fontWeight: FontWeight.w300,fontSize: 20.0),semanticsLabel: "PickUpLocation",),
+                                        ],
+                                      ),
+                                      Divider(indent: 50,),
+                                      Row(
+                                        mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                        movingServiceModel.accepted?Icon(Icons.check_circle):Container(
+                                            margin: EdgeInsets.all(15.0),
+                                            width: 10,
+                                              height: 10,
+                                            child:CircularProgressIndicator()),
+                                            Text(movingServiceModel.accepted?"Request Accepted":"Your request is being processed",style: TextStyle(fontSize: 12.0,fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                      Divider(),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: RichText(text: TextSpan(
+                                            text: "Supervisor Alloted : ",
+                                            style: TextStyle(fontWeight: FontWeight.w300,color: Colors.black,fontSize: 15.0),
+                                            children:[
+                                              TextSpan(text:movingData.data.documents[0].data["SuperVisorName"].toString().toUpperCase() ,
+                                                  style: TextStyle(fontWeight: FontWeight.bold))
+                                            ]
+                                        ),),
+                                      ),
+
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: RichText(text: TextSpan(
+                                            text: "Survey Time : ",
+                                            style: TextStyle(fontWeight: FontWeight.w300,color: Colors.black,fontSize: 15.0),
+                                            children:[
+                                              TextSpan(text:movingServiceModel.surveyTime.toUpperCase() ,
+                                                  style: TextStyle(fontWeight: FontWeight.bold))
+                                            ]
+                                        ),),
+                                      ),
+
                                     ],
                                   ),
                                 ),
-                              )
-                            : Center(
-                                child: LinearProgressIndicator(),
                               );
-                      }),
-                );
+                            } else
+                              return Center(
+                                child: Container(
+                                    width: 50,
+                                    height: 50,
+                                    child: CircularProgressIndicator()),
+                              );
+                          }),
+                    );
+            }
+          );
         });
   }
+
+  Widget initialDataWidget() {}
 }
